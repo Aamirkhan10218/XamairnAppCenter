71899281b66881d4b5a3123c6a2ff695f372be61
Aamir Khan <Aamir Khan@DESKTOP-T0T75J6>
2020-10-30 21:48:27 +0500

    Add project files.

---
+++ XamairnAppCenter/XamairnAppCenter/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using XamairnAppCenter.ViewModels;
+
+namespace XamairnAppCenter.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
