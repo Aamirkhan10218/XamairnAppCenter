88c693ce3031b306f270329a4d66dd7339171216
Aamir Khan <Aamir Khan@DESKTOP-T0T75J6>
2020-11-29 14:29:09 +0500

    Open Browser

--- XamairnAppCenter/XamairnAppCenter/Views/AboutPage.xaml.cs
+++ XamairnAppCenter/XamairnAppCenter/Views/AboutPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,6 +13,20 @@ namespace XamairnAppCenter.Views
             InitializeComponent();
             api.Source = "http://www.geenraltesting.somee.com/api/Report/GetReport?ReportName=UserDetails";
           //  google.Source = "https://www.google.com/";
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            string url = "http://www.geenraltesting.somee.com/api/Report/GetReport?ReportName=UserDetails";
+                try
+                {
+                    await Browser.OpenAsync(url, BrowserLaunchMode.SystemPreferred);
+                }
+                catch (Exception ex)
+                {
+                    // An unexpected error occured. No browser may be installed on the device.
+                }
+           
         }
     }
 }
